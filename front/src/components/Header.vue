1ff99ebd6d989159832f6ff7b024b7fd8955864a
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-03-21 23:31:56 +0100

    add the balise (nav) between the list, add a text-decoration none and center the nav in the component Header.vue

--- front/src/components/Header.vue
+++ front/src/components/Header.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="container">
+        <nav>
         <ul>
             <li><a href="#">Mariage</a></li>
             <li><a href="#">Portrait</a></li>
@@ -7,12 +8,21 @@
             <li><a href="#">A propos</a></li>
             <li><a href="#">Me contacter</a></li>
         </ul>
+        </nav>
     </div>
 </template>
 
 <style>
+nav{
+    display: flex;
+    justify-content: center
+}
 ul{
     display: flex;
     list-style-type: none;
+}
+a:link{
+    text-decoration: none;
+
 }
 </style>
