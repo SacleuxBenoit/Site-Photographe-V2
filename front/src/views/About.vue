4e8b13988c6ba950c4f36538ce96067c984b928f
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-03-22 22:37:01 +0100

    add a H1, align the content center, and add some lorem ipsum in the views/About.vue

--- front/src/views/About.vue
+++ front/src/views/About.vue
@@ -1,4 +1,13 @@
 <template>
   <div class="about">
+    <h1>Qui suis-je ?</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore velit quis omnis voluptatem placeat eum, pariatur perspiciatis qui minus, maiores dicta voluptatum ab esse provident recusandae veniam tempora ratione numquam? Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur excepturi, molestiae ab necessitatibus repellendus, amet nulla fugiat sequi delectus nemo cupiditate, eum ad. Temporibus qui rem corrupti exercitationem delectus nobis. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint aliquid est neque sit dolorem voluptatum! Ratione quam fuga itaque nemo odio velit rem, pariatur doloremque sit asperiores consequuntur atque libero? Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur dolorem magnam blanditiis suscipit et, error sint ullam nisi dolores vel molestias. Beatae maiores cupiditate corporis qui magni fugit, enim officiis. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore dignissimos labore velit possimus placeat dicta. Itaque in vero soluta labore, dignissimos hic repellendus laboriosam illo ut temporibus, quisquam tempore? Veritatis?</p>
   </div>
 </template>
+
+<style>
+.about{
+  text-align: center;
+  padding-top:3rem;
+}
+</style>
